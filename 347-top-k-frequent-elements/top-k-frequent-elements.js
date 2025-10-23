c36dc003a76f7035f9e3e72cd1f3e2d8b6e76d18
Ruposhi Gorai <ruposhigorai09@gmail.com>
2025-10-23 10:39:37 +0530

    Time: 10 ms (74.04%) | Memory: 58.8 MB (69.53%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -2,20 +2,14 @@
 var topKFrequent = function(nums, k) {
 
   let map = {};
-
-  // Step 1️⃣: Count frequency
   nums.forEach(v => {
-    map[v] = (map[v] || 0) + 1;
+    map[v] = (map[v] ?? 0) + 1;
   });
-
-  // Step 2️⃣: Convert to array of [key, value] pairs
   let newArr = Object.entries(map); // [['1',3], ['2',2], ['3',4]]
 
-  // Step 3️⃣: Sort by value (frequency) in descending order
-  newArr.sort((a, b) => b[1] - a[1]);
+  newArr.sort((a, b) => b[1] - a[1]); //sort in decending order
 
-  // Step 4️⃣: Take first k keys
-  let result = newArr.slice(0, k).map(pair => Number(pair[0]));
+  let result = newArr.slice(0, k).map(pair => Number(pair[0])); //get keys of first k elements
 
   console.log(result);
   return result;
