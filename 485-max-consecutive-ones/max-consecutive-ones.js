884825b016ec1f692a8eb7178ec4f9c9c1e936d6
Ruposhi Gorai <ruposhigorai09@gmail.com>
2025-12-03 11:32:26 +0530

    Time: 1 ms (91.86%) | Memory: 58 MB (98.03%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.js
+++ 485-max-consecutive-ones/max-consecutive-ones.js
@@ -1,19 +1,23 @@
-
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
 var findMaxConsecutiveOnes = function(nums) {
-let maxCount = 0;
-let count = 0 ;
+     let maxCount = 0;
+     let count = 0 ;
+     for(let i = 0; i < nums.length ; i++){
+        if(nums[i] === 1){
+            count++
+        }else{
+            if(maxCount < count){
+            maxCount = count ;
 
-for(let i = 0; i < nums.length; i++){
-    if(nums[i] === 1 ){
-        count++;
-        maxCount = Math.max(maxCount , count)
-
-    }else{
-        count = 0
-    } 
-
-}
-    return maxCount
-    
-    
+            }
+            count = 0;
+        }
+     }
+     if(maxCount < count){
+        maxCount = count;
+     }
+    return maxCount;
 };
